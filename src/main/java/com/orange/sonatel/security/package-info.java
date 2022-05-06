4513a14ffe90e7f285bd4a03279e54ca39a54db2
Astou niang <niangastou1997@gmail.com>
2022-05-06 16:28:25 +0000

    gateway skeleton

---
+++ src/main/java/com/orange/sonatel/security/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Spring Security configuration.
+ */
+package com.orange.sonatel.security;
---
+++ src/main/java/com/orange/sonatel/web/rest/vm/ManagedUserVM.java
@@ -0,0 +1,19 @@
+package com.orange.sonatel.web.rest.vm;
+
+import com.orange.sonatel.service.dto.AdminUserDTO;
+
+/**
+ * View Model extending the AdminUserDTO, which is meant to be used in the user management UI.
+ */
+public class ManagedUserVM extends AdminUserDTO {
+
+    public ManagedUserVM() {
+        // Empty constructor needed for Jackson.
+    }
+
+    // prettier-ignore
+    @Override
+    public String toString() {
+        return "ManagedUserVM{" + super.toString() + "} ";
+    }
+}
